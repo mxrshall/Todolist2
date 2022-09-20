5d609227de7e65e1b9956c19c539ca3ec8d3688c
mxrshall <martin22.krajcovic@gmail.com>
2022-09-20 19:44:17 +0200

    Render

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var add = document.getElementById("add");
 var uloha = document.getElementById("uloha");
 var storage = [];
+
+var fromprestorage = JSON.parse(localStorage.getItem("uloha"));
+
 add.addEventListener("click", function () {
     storage.push(uloha.value);
     prestorage = JSON.stringify(storage);
     localStorage.setItem("uloha", prestorage);
-    uloha.value = "";
+    render()
 });
-var fromprestorage = JSON.parse(localStorage.getItem("uloha"));
+
 fromprestorage.forEach(function (uloha) {
     var container = document.createElement("div");
     var paragraph = document.createElement("p");
@@ -16,3 +19,13 @@ fromprestorage.forEach(function (uloha) {
     document.body.appendChild(container);
     container.appendChild(paragraph);
 });
+
+function render(){
+    var container = document.createElement("div");
+    var paragraph = document.createElement("p");
+    paragraph.textContent = uloha.value;
+    container.setAttribute("class", "container");
+    document.body.appendChild(container);
+    container.appendChild(paragraph);
+    uloha.value = ""
+}
--- index.ts
+++ index.ts
@@ -1,27 +1,33 @@
-const add = document.getElementById("add")
-let uloha = document.getElementById("uloha")
+var add = document.getElementById("add");
+var uloha = document.getElementById("uloha");
+var storage = [];
 
-let storage = []
+var fromprestorage = JSON.parse(localStorage.getItem("uloha"));
 
-add.addEventListener("click", function(){
-    storage.push(uloha.value)
-    prestorage = JSON.stringify(storage)
-    localStorage.setItem("uloha", prestorage)
+add.addEventListener("click", function () {
+    storage.push(uloha.value);
+    prestorage = JSON.stringify(storage);
+    localStorage.setItem("uloha", prestorage);
+    render()
+});
+
+fromprestorage.forEach(function (uloha) {
+    var container = document.createElement("div");
+    var paragraph = document.createElement("p");
+    paragraph.textContent = uloha;
+    container.setAttribute("class", "container");
+    document.body.appendChild(container);
+    container.appendChild(paragraph);
+});
+
+function render(){
+    var container = document.createElement("div");
+    var paragraph = document.createElement("p");
+    paragraph.textContent = uloha.value;
+    container.setAttribute("class", "container");
+    document.body.appendChild(container);
+    container.appendChild(paragraph);
     uloha.value = ""
-})
-
-let fromprestorage = JSON.parse(localStorage.getItem("uloha"))
-
-fromprestorage.forEach(function(uloha){
-        let container = document.createElement("div")
-        let paragraph = document.createElement("p")
-    
-        paragraph.textContent = uloha
-    
-        container.setAttribute("class", "container")
-        document.body.appendChild(container)
-        container.appendChild(paragraph)
-    })
 }
 
 
